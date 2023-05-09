e0ffaea21032c468cdb53bde12860d57e5f13dc2
akash-0504 <58282423+akash-0504@users.noreply.github.com>
2023-05-09 18:53:24 +0530

    springboot 14 video

--- src/main/java/com/jpa/test/BootjpaexampleApplication.java
+++ src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -1,6 +1,7 @@
 package com.jpa.test;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -18,13 +19,13 @@ public class BootjpaexampleApplication {
 		ApplicationContext context = SpringApplication.run(BootjpaexampleApplication.class, args);
 		UserRepository userRepository = context.getBean(UserRepository.class);
 
-		/*
-		 * User user = new User(); user.setName("akash bairagi");
-		 * user.setCity("indore"); user.setStatus("java developer");
-		 * 
-		 * User user2 = userRepository.save(user); System.out.println(user2);
-		 */
-		// create object of user
+		
+//		  User user = new User(); user.setName("akash bairagi");
+//		  user.setCity("indore"); user.setStatus("java developer");
+//		  
+//		  User user2 = userRepository.save(user); System.out.println(user2);
+		 
+		 //create object of user
 //		User user1 = new User();
 //		user1.setName("anil");
 //		user1.setCity("pune");
@@ -34,30 +35,33 @@ public class BootjpaexampleApplication {
 //		user2.setName("veer");
 //		user2.setCity("Mumbai");
 //		user2.setStatus("c# developer");
-//
-//		// saving single user
-//		// User resultUser = userRepository.save(user2);
-//		List<User> users = List.of(user1, user2);
+
+		// saving single user
+		// User resultUser = userRepository.save(user2);
+		//List<User> users = List.of(user1, user2);
 //		
+		
 //		//save multiple objects
-//		Iterable<User> result = userRepository.saveAll(users);
+//      	Iterable<User> result = userRepository.saveAll(users);
 //
 //		result.forEach(user -> {
 //			System.out.println(user);
 //
 //		});
-//
+
 //		// System.out.println("saved user" + resultUser);
 //		System.out.println("done");
 
+		
 		// update the user of id 53
-
 //		Optional<User> optional = userRepository.findById(53);
 //		User user = optional.get();
 //		user.setName("raj kapoor");
 //		User result = userRepository.save(user);
 //		System.out.println(result);
 
+		
+		
 		//how to get data 
 		//findById(id) - return optional containing your data 
 //		Iterable<User> itr = userRepository.findAll();
@@ -80,10 +84,17 @@ public class BootjpaexampleApplication {
         
 		//deleting by all users
 		
-		Iterable<User> alluser = userRepository.findAll();
+//		Iterable<User> alluser = userRepository.findAll();
+//		
+//		alluser.forEach(user->System.out.println(user));
+//		
+//		userRepository.deleteAll(alluser);
 		
-		alluser.forEach(user->System.out.println(user));
 		
-		userRepository.deleteAll(alluser);
+		
+		
+		  List<User> users =  userRepository.findByName("anil");
+		  
+		  users.forEach(e->System.out.println(e));
 	}
 }
